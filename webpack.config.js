6df0136509174cca48a70002b99a0ab7c6d5c18a
Samuel Zelalem <zsami9017@gmail.com>
2025-07-19 23:37:15 -0700

    CREATING REACT APP HELLO WORLD

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,62 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
-    clean: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$|\.jsx$/,
-        exclude: /node_modules/,
-        use: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|jpg|svg|gif)$/,
-        type: "asset/resource"
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: "./public/index.html" }),
-    new Dotenv()
-  ],
-  devServer: {
-    static: "./dist",
-    port: 3000,
-    open: true,
-    hot: true
-  },
-  mode: "development"
+module.exports = (env) => {
+  return {
+    mode: env.NODE_ENV || 'development',
+
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'images',
+              },
+            },
+          ],
+        },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new Dotenv({
+        path: `./.env.${env.NODE_ENV || 'development'}`,
+      }),
+    ],
+
+    devServer: {
+      historyApiFallback: true,
+      port: 3000,
+      open: true,
+      hot: true,
+    },
+
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  };
 };
